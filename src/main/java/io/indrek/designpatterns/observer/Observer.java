5ecccbf00583e37e2e6382e92379b76544394987
= <=>
2016-06-22 16:24:55 +0300

    initial commit

---
+++ src/main/java/io/indrek/designpatterns/observer/Observer.java
@@ -0,0 +1,5 @@
+package io.indrek.designpatterns.observer;
+
+public interface Observer {
+    void update();
+}
---
+++ src/main/java/io/indrek/designpatterns/observer/Subject.java
@@ -0,0 +1,7 @@
+package io.indrek.designpatterns.observer;
+
+public interface Subject {
+    void register(Observer observer);
+    void remove(Observer observer);
+    void notifyObservers();
+}
